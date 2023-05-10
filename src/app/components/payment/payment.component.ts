80576ff3aeb64aac1f34f51db48b9cf0a0d62ca9
fhrr-sht <54956154+fhrr-sht@users.noreply.github.com>
2023-05-10 12:17:13 +0300

    Add Calender and Payment components

---
+++ src/app/components/payment/payment.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
+import { Reservation } from 'src/app/models/reservation';
+import { CalendarService } from 'src/app/services/calendar.service';
+import { ToastrService } from 'ngx-toastr';
+import { Room } from 'src/app/models/room';
+import { Customer } from 'src/app/models/customer';
+import { ActivatedRoute, Router } from '@angular/router';
+
+declare var paypal;
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
+})
+export class PaymentComponent implements OnInit {
+  @ViewChild('paypal', { static: true }) paypalElement: ElementRef;
+
+  reservation: Reservation;
+  room: Room;
+  customer: Customer;
+
+  constructor(private calendarService: CalendarService, private toastrService: ToastrService, private router: Router) {
+    this.reservation = JSON.parse(localStorage.getItem('reservation'));
+    this.room = JSON.parse(localStorage.getItem('room'));
+  }
+
+  ngOnInit() {
+    paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: "Room: " + this.room.RehersalRoomName,
+                amount: {
+                  currency_code: 'DKK',
+                  value: this.reservation.Total
+                }
+              }
+            ]
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          this.calendarService.createReservation(this.reservation).subscribe(
+            () => {
+              this.toastrService.success("Payment successfully done!");
+              this.router.navigate(['/room/' + this.room.PathURL]);
+            },
+            (error) => {
+              this.toastrService.error("Some error occured while processing the transaction!");
+            })
+
+        },
+        onError: err => {
+          this.toastrService.error(err);
+        }
+      })
+      .render(this.paypalElement.nativeElement);
+  }
+}
+
